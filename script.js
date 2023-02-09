65e7b5b963d8bd85f1d3e131c89b4614688889b6
kpetrosky <kpetrosky36@gmail.com>
2023-02-08 17:59:08 -0700

    worked with tutor to figure out where to start

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log ("made it inside function")
+  // prompt user how long the passwords needs to be-how many characters
+  //var userChoice = window.prompt("Enter R, P, or S:"); ---how long password
+  //ask user if want upper/lower/special characters/number etc
+  // var playAgain = window.confirm("Play again?");--- asking for each types of characters
+  //based on users choices, i need to create a password that fills all those needs that are checked about
+  // Get random index from array of options (help set up random index)
+ // var index = Math.floor(Math.random() * options.length);
+ // var computerChoice = options[index];
+  //return created password(handoff)
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
